969bac0113c29328eaac8f50bf3feb1c6b8b573c
g0dsCookie <g0dscookie@cookieprojects.de>
2019-07-05 19:31:58 +0000

    Shortened sleep before first statusline

--- pystatus.py
+++ pystatus.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     # TODO: Wait until every plugin has set atleast full_text set
     # Just a quick fix for full_text unset
     # on first line sent
-    time.sleep(1)
+    time.sleep(0.1)
 
     statusline.start()
     while statusline.is_open:
